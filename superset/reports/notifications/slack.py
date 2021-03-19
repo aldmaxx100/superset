7138c219e97e18b350da2492b510cbc6cb9fec69
aldmaxx100 <aldrin.machado@gmail.com>
2021-03-19 18:08:18 +0530

    flock patch

--- superset/reports/notifications/slack.py
+++ superset/reports/notifications/slack.py
@@ -19,12 +19,14 @@ import json
 import logging
 from io import IOBase
 from typing import Optional, Union
+import random
 
 from flask_babel import gettext as __
 from retry.api import retry
 from slack import WebClient
 from slack.errors import SlackApiError, SlackClientError
-
+import requests
+import boto3
 from superset import app
 from superset.models.reports import ReportRecipientType
 from superset.reports.notifications.base import BaseNotification
@@ -73,8 +75,14 @@ class SlackNotification(BaseNotification):  # pylint: disable=too-few-public-met
             return self._content.screenshot.image
         return None
 
-    @retry(SlackApiError, delay=10, backoff=2, tries=5)
+
+
+
+
     def send(self) -> None:
+        if self.sendToWebhook():
+            logging.info("Report sent to flock")
+            return
         file = self._get_inline_screenshot()
         channel = self._get_channel()
         body = self._get_body()
@@ -93,3 +101,103 @@ class SlackNotification(BaseNotification):  # pylint: disable=too-few-public-met
             logger.info("Report sent to slack")
         except SlackClientError as ex:
             raise NotificationError(ex)
+
+
+    def uploadAndPresign(self):
+        try:
+            file = self._get_inline_screenshot()
+
+            s3 = boto3.resource(service_name='s3',
+                                aws_access_key_id=app.config['IMEDIA_AWS_ACCESS_KEY'],
+                                aws_secret_access_key=app.config['IMEDIA_AWS_SECRET_KEY'])
+            choices = [chr(i + 97) for i in range(26)]
+            choosed = [random.choice(choices) for i in
+                       range(random.choice([5, 6, 7, 8, 9, 10]))]
+            filename=''.join(choosed)
+            s3.meta.client.upload_file(Filename=file, Bucket=app.config['IMEDIA_SUPERSET_BUCKET'],
+                                       Key=filename)
+            response = s3.meta.client.generate_presigned_url('get_object',
+                                                             Params={
+                                                                 'Bucket': app.config['IMEDIA_SUPERSET_BUCKET'],
+                                                                 'Key': filename},
+
+                                                             ExpiresIn=3600)
+
+            return response
+        except Exception as e:
+            logging.error(e.__str__)
+            raise Exception('Error in Presignning url generation')
+
+    def generatePayload(self,preSignedUrl):
+
+        try:
+            send_template = {
+                'text': '',
+
+                'attachments': [{
+                    'title': "",
+                    'views': {
+                        "image": {
+                            "original": {
+                                "src": "",
+                                "width": 400, "height": 400}
+                        }
+                    }
+                }]
+            }
+
+            alertname = self._content.name
+            url = self._content.screenshot.url
+            send_template['text']=url+'|Explore More in SuperSet'
+            send_template['attachments'][0]['title']=alertname
+            send_template['attachments'][0]['views']['image']['original']['src']=preSignedUrl
+            return send_template
+        except Exception as e:
+            logging.error(e.__str__())
+            raise Exception('Error in Generating payload')
+
+
+    def sendToFlock(self,flockUrl,payload):
+        try:
+            count=0
+            while count<=5:
+                response=requests.post(flockUrl,json=payload)
+                if response.status_code==200:
+                    logging.info('Flock alert sent successfully')
+                    break
+                else:
+                    count+=1
+
+
+
+        except Exception as e:
+            logging.error(e.__str__())
+
+
+
+
+    def sendToWebhook(self):
+        try:
+            channel=self._get_channel()
+            if 'webhook:'==channel[:8]:
+                preSignedUrl=self.uploadAndPresign()
+                payload=self.generatePayload(preSignedUrl)
+
+                flockurl=channel[8:].split(',')
+
+                for flock in flockurl:
+                    self.sendToFlock(flock,payload)
+
+
+                return True
+            else:
+                return False
+        except Exception as e:
+            logging.error(e.__str__())
+            return False
+
+
+
+
+
+
